44196dbb40084109474f83c7b5b416b91c5bfa22
Stefan_mit_Trekstor_repariert_und_Mgit_1.5.6 <backebackekuchen16@gmail.com>
2020-10-08 14:53:24 +0200

    GPIO14 und GPIO15 als UART1

--- ok02/source/main.s
+++ ok02/source/main.s
@@ -4,6 +4,7 @@
 //3 GPIO15 in GPIO4 ausgeben
 //4 und auch in GPIO14 ausgeben
 //5 als nächstes GPIO4 auf GPIO2
+//6 GPIO14 und GPIO15 als UART1
 
 /******************************************************************************
 *	main.s
@@ -44,79 +45,103 @@ ldr r0,=0x20200000
 *				to decrement to 0, allowing us to create a delay.
 */
 
-/*
-* Set the GPIO function select.
-*/
-mov r1,#1 //5
-lsl r1,#6 //5 war vorher #12 und #18
-str r1,[r0,#0] //1 war vorher #4
+//6 GPIO2 auf Funktion OUT einstellen
+mov r1,#1      //6 Funktion OUT
+lsl r1,#6      //6 GPIO2
+str r1,[r0,#0] //6 Funktion auswählen
 
-mov r1,#1  //5
-lsl r1,#12 //5 war vorher #18
-str r1,[r0,#4] //4 wieder #4, zusätzlich wegen GPIO14
-
-/* 
-* Set the 16th bit of r1.
-*/
+//6 GPIO2 paarmal schalten
 mov r1,#1
-lsl r1,#2 //5 war vorher #4 und #16 
-
+lsl r1,#2 //6 Bitposition für  GPIO2
 mov r3,#0x0A //2 Anzahl Wiederholungen
-
-/* NEW
-* Label the next line loop$ for the infinite looping
-*/
 loop$: 
-
-/*
-* Set GPIO 16 to low, causing the LED to turn on.
-*/
 str r1,[r0,#40]
-
-/* NEW
-* Now, to create a delay, we busy the processor on a pointless quest to 
-* decrement the number 0x3F0000 to 0!
-*/
 mov r2,#0x1F0000 //2 etwas schneller
 wait1$:
 	sub r2,#1
 	cmp r2,#0
 	bne wait1$
-
-/* NEW
-* Set GPIO 16 to high, causing the LED to turn off.
-*/
 str r1,[r0,#28]
-
-/* NEW
-* Wait once more.
-*/
 mov r2,#0x1F0000 //2 etwas schneller
 wait2$:
 	sub r2,#1
 	cmp r2,#0
 	bne wait2$
-
-/*
-* Loop over this process forevermore
-*/
-
 sub r3,#1 //2 nur wenige Wiederholungen
 cmp r3,#0 //2
 bne loop$ //2
 
 
-mov r2,#1    //4
-lsl r2,#14   //4 Bitposition von GPIO14
-orr  r1,r1,r2 //4 gleich mit der Bitposition von GPIO4 verknüpfen
+//6 GPIO14 und GPIO15 auf Funktion UART1 einstellen
+mov r1,#2      //6 ALT5
+lsl r1,#12     //6 nach GPIO14 schieben
+mov r2,#2      //6 nochmal ALT5
+lsl r2,#15     //6 nach GPIO15 schieben
+orr r1,r1,r2   //6 beide zusammenfassen 
+str r1,[r0,#4] //6 und Funktion auswählen
 
-mov r4,#1  //3
-lsl r4,#15 //3 Pin GPIO15
+//6 hierher kommt noch Pull up für GPIO14 und GPIO15
 
-loop2$:   //2
-ldr   r5,[r0,#0x34] //3 GPIO15 lesen
-and   r5,r5,r4    //3 Pin
+//6 UART1 Adressen:
+ldr r0,=0x20215000 //AUXIRQ
+/*6
+#define AUX_ENABLES     (PBASE+0x00215004)
+#define AUX_MU_IO_REG   (PBASE+0x00215040)
+#define AUX_MU_IER_REG  (PBASE+0x00215044)
+#define AUX_MU_IIR_REG  (PBASE+0x00215048)
+#define AUX_MU_LCR_REG  (PBASE+0x0021504C)
+#define AUX_MU_MCR_REG  (PBASE+0x00215050)
+#define AUX_MU_LSR_REG  (PBASE+0x00215054)
+#define AUX_MU_MSR_REG  (PBASE+0x00215058)
+#define AUX_MU_SCRATCH  (PBASE+0x0021505C)
+#define AUX_MU_CNTL_REG (PBASE+0x00215060)
+#define AUX_MU_STAT_REG (PBASE+0x00215064)
+#define AUX_MU_BAUD_REG (PBASE+0x00215068)
+6*/
+
+//6 UART1 konfigurieren (Baudrate und dies und das)
+mov r3,#1
+str r3,[r2,#0x04] //6 put32(AUX_ENABLES,1);    "Enable mini uart"
+mov r3,#0
+str r3,[r2,#0x60] //6 put32(AUX_MU_CNTL_REG,0); "Disable flow control and TX and RX"
+mov r3,#0
+str r3,[r2,#0x44] //6 put32(AUX_MU_IER_REG,0);  "Disable receive and transmit interrupts"
+mov r3,#1         //6 3 ist nicht ok
+str r3,[r2,#0x4C] //6 put32(AUX_MU_LCR_REG,3);  "Enable 8 bit mode"
+mov r3,#0
+str r3,[r2,#0x50] //6 put32(AUX_MU_MCR_REG,0);   "Set RTS line to be always high"
+mov r3,#270
+str r3,[r2,#0x68] //6 put32(AUX_MU_BAUD_REG,270); "Set baud rate to 115200"
+mov r3,#3
+str r3,[r2,#0x60] //6 put32(AUX_MU_CNTL_REG,3);   "Finally, enable transmitter and receiver"
+
+
+//6 hier nun RXD lesen und auf GPIO2 und TXD ausgeben
+/*6
+void uart_send ( char c )
+{
+    while(1) {
+        if(get32(AUX_MU_LSR_REG)&0x20) 
+            break;
+    }
+    put32(AUX_MU_IO_REG,c);
+}
+
+char uart_recv ( void )
+{
+    while(1) {
+        if(get32(AUX_MU_LSR_REG)&0x01) 
+            break;
+    }
+    return(get32(AUX_MU_IO_REG)&0xFF);
+}
+
+6*/
+
+loop2$:  //2
+ldr   r5,[r2,#0x54] //6 RXD lesen
+and   r5,r5,#1    //6 Bit0
 cmp   r5,#0       //3
-streq r1,[r0,#40] //3 und in GPIO4 ausgeben, LED an 3,3V 
+streq r1,[r0,#40] //3 und in GPIO2 ausgeben, LED an 3,3V 
 strne r1,[r0,#28] //3
 b loop2$ //2 Ende Versuch 2
